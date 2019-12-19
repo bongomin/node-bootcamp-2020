f2372993fda5042c76545d88fe8464a81950470f
bongomin <candanobongomindaniel@gmail.com>
2019-12-19 13:14:02 +0300

    feat(simple web server):
    - created a simple web server using the http pacrkage

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const fs = require('fs')
+const http = require('http')
+
+// Files //////
+// const textout = "welcome danny"
+// const file = fs.writeFileSync('input.txt', 'welcome here my niga', (err, data) => {
+
+//    console.log(data('welcome'))
+// });
 
 
-const textout = "welcome danny"
-const file = fs.writeFileSync('input.txt', 'welcome here my niga', (err, data) => {
-
-   console.log(data('welcome'))
+// Server////
+const server = http.createServer((req, res) => {
+   console.log(req);
+   res.end('Hello from the server from Daniel !')
 });
+
+
+server.listen(3000, () => {
+   console.log('the application is running on port 3000');
+})
+
