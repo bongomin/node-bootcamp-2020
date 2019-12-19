e6a28d78e54d07ebf5a439fce5b1b1adc37c34fb
bongomin <candanobongomindaniel@gmail.com>
2019-12-19 12:46:08 +0300

    feat(reading files usining asyn):

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const fs = require('fs')
 
 
+
+// reading files
 const textout = "welcome danny"
 const file = fs.writeFileSync('input.txt', 'welcome here my niga', (err, data) => {
 
    console.log(data('welcome'))
 });
+
+
+// non-blocking asynchronous
+fs.readFile('filename', 'utf-8', (err, data) => {
+   console.log(data)
+})
+
+
+
